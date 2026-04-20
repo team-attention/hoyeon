5f8154a544b29f5457eafc8715cff3748ddda144
yansfil <tansfil@gmail.com>
2026-04-20 18:01:19 +0900

    chore(release): bump version to 1.6.1

--- cli/dist/cli.js
+++ cli/dist/cli.js
@@ -7700,7 +7700,7 @@ async function main() {
     process.exit(0);
   }
   if (args[0] === "--version") {
-    const version = true ? "1.6.0" : "dev";
+    const version = true ? "1.6.1" : "dev";
     process.stdout.write(`hoyeon-cli v${version}
 `);
     process.exit(0);
